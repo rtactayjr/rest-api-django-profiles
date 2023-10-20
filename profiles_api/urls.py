89e1c35e9d24616c0ba8edf2efc64519580232c3
r-tactay <rolandtactay571@gmail.com>
2023-10-20 19:45:06 +0800

    Update HelloViewSet class: add create, retrieve, update, partial and destroy functions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,26 +2,16 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from . import views
+from profiles_api import views
 
 
-# router = DefaultRouter()
-# router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 # router.register('profile', views.UserProfileViewSet)
 # router.register('feed', views.UserProfileFeedViewSet)
 
-'''
-    This part specifies the view class that will be used to handle the request.
-    In Django Rest Framework, views are implemented as Python classes.
-
-    The as_view() method is a method provided by DRF that converts
-    the view class into a callable view function that can be used in URL patterns
-'''
-
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
-
     # path('login/', views.UserLoginApiView.as_view()),
-    #
-    # path('', include(router.urls))
+    path('', include(router.urls))
 ]
