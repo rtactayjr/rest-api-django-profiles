4ab9dae4e2d369611443f4b62f72a55792e18fcf
r-tactay <rolandtactay571@gmail.com>
2023-10-22 12:37:03 +0800

    Create ViewSet for our profile feed item

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,12 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
-
+# for ViewSet
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
-# router.register('feed', views.UserProfileFeedViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
+# for APIVIEW
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
