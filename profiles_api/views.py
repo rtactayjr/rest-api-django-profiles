79d55f41fb2d9a1571c68ead8adec11d40f8a9b6
r-tactay <rolandtactay571@gmail.com>
2023-10-22 12:54:08 +0800

    add permissions for Feed API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -138,6 +140,8 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
 
     def perform_create(self, serializer):
         """Sets the user profile to the logged in user"""
